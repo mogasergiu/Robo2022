aa69edf9198a2b5ad9e863f83384dc06c51de9ae
Sergiu Moga <sergiu.moga@protonmail.com>
2022-10-30 00:18:41 +0300

    main: Test and measure sensor and motor driver performance and parameters
    Signed-off-by: Sergiu Moga <sergiu.moga@protonmail.com>

--- main.c
+++ main.c
@@ -12,15 +12,11 @@
 
 gpio_t gpios[] = {
     {
-        .num = D18_GPIO12_A4_SDA,
-        .func = GPIO_FUNC_I2C,
-    },
-    {
-        .num = D19_GPIO13_A5_SCL,
-        .func = GPIO_FUNC_I2C,
-    },
-    {
         .num = D2_GPIO25,
+        .func = GPIO_FUNC_PWM,
+    },
+    {
+        .num = D3_GPIO15,
         .func = GPIO_FUNC_PWM,
     },
     {
@@ -70,7 +66,8 @@ static void pwm_gpio_enable(gpio_t *gpio)
     channel = pwm_gpio_to_channel(gpio->num);
 
     pwm_set_wrap(slice_num, DEFAULUT_PWM_WRAP);
-    pwm_set_chan_level(slice_num, channel, 169);
+    pwm_set_chan_level(slice_num, channel, 130);
+    pwm_set_clkdiv(slice_num, 46.f);
     pwm_set_enabled(slice_num, true);
 }
 
@@ -85,6 +82,20 @@ static void pwm_gpio_set_duty_cycle(gpio_t *gpio, uint percent)
     pwm_set_chan_level(slice_num, channel, percent_to_wrap);
 }
 
+void gpio_irq_handler(uint gpio_num, uint32_t event_mask)
+{
+    switch (gpio_num) {
+    case D12_GPIO4_CIPO:
+        printf("D12 %u\n", time_us_32());
+        gpio_put(D5_GPIO17, 1);
+        break;
+    case D11_GPIO7_COPI:
+        printf("D11 %u\n", time_us_32());
+        gpio_put(D5_GPIO17, 0);
+        break;
+    }
+}
+
 static int init_all()
 {
     int ret;
@@ -94,11 +105,9 @@ static int init_all()
      */
     stdio_init_all();
 
-    printf("PULA MEA PDLSAPD");
 #if DEBUG
-    sleep_ms(5000);
+    sleep_ms(2000);
 #endif
-    printf("PULA MEA PDLSAPD");
 
     i2c_init(i2c_default, 400 * 1000);
 
@@ -108,17 +117,39 @@ static int init_all()
         return ret;
     }
 
+    gpio_init(D11_GPIO7_COPI);
+    gpio_set_function(D11_GPIO7_COPI, GPIO_FUNC_PIO0);
+    gpio_set_irq_enabled_with_callback(D11_GPIO7_COPI, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
+    gpio_init(D12_GPIO4_CIPO);
+    gpio_set_function(D12_GPIO4_CIPO, GPIO_FUNC_PIO0);
+    gpio_set_irq_enabled(D12_GPIO4_CIPO, GPIO_IRQ_EDGE_RISE, true);
+
+    set_sys_clock_khz(220000, true);
+
     return 0;
 }
 
 int main() {
     init_all();
 
+    gpio_init(D3_GPIO15);
+    gpio_set_dir(D3_GPIO15, GPIO_OUT);
+    gpio_set_function(D3_GPIO15, GPIO_FUNC_PWM);
+    pwm_gpio_enable(&gpios[1]);
+
+    gpio_init(D5_GPIO17);
+    gpio_set_dir(D5_GPIO17, GPIO_OUT);
+    gpio_put(D5_GPIO17, 0);
+
+    pwm_config cfg = pwm_get_default_config();
+    printf("div: %u\n", cfg.div);
+
     while (true) {
         //printf("hi\n");
         pr_debug("g.x %d\ng.y %d\ng.z %d\na.x %d\na.y %d\na.z %d\n",
                 lsm6dsox.gyro->get_x(), lsm6dsox.gyro->get_y(), lsm6dsox.gyro->get_z(),
                 lsm6dsox.acc->get_x(), lsm6dsox.acc->get_y(), lsm6dsox.acc->get_z());
+        sleep_ms(1000);
     }
     return 0;
 }
