mogasergiu/Robo2022

commit 9521b9e9e8dca570cfef538d44ffbef3c5db2549

Add generic I2C operations
Signed-off-by: Sergiu Moga <sergiu.moga@protonmail.com>

drivers/i2c.c
@@ -1,0 +1,28 @@
+#include <i2c.h>
+#include <debug.h>
+
+int i2c_write_byte(uint8_t saddr, uint8_t reg, uint8_t value)
+{
+    uint8_t msg[2];
+    msg[0] = reg;
+    msg[1] = value;
+
+    pr_debug("%s:%u Writing to saddr %x reg %x value %x", saddr, reg, value);
+
+    return i2c_write_blocking(i2c_default, saddr, msg, sizeof(msg), false);
+}
+
+int i2c_read_byte(uint8_t saddr, uint8_t reg, uint8_t *value)
+{
+    int ret;
+
+    pr_debug("%s:%u Writing to saddr %x reg %x", saddr, reg);
+    ret = i2c_write_blocking(i2c_default, saddr, &reg, sizeof(reg), false);
+    if (ret <= 0)
+        return -1;
+
+    pr_debug("%s:%u Reading from saddr %x reg %x", saddr, reg);
+    ret = i2c_read_blocking(i2c_default, saddr, value, sizeof(*value), false);
+    if (ret <= 0)
+        return -1;
+}

include/i2c.h
@@ -1,0 +1,23 @@
+#include "pico/stdlib.h"
+#include "hardware/i2c.h"
+
+#ifndef I2C_H
+#define I2C_H
+
+typedef struct {
+    int (*probe)();
+    int (*read_byte)(uint8_t saddr, uint8_t reg, uint8_t *value);
+    int (*write_byte)(uint8_t saddr, uint8_t reg, uint8_t value);
+    int (*read_bytes)(uint8_t saddr, uint8_t reg, uint8_t *buf, size_t len);
+    int (*write_bytes)(uint8_t saddr, uint8_t reg, uint8_t *buf, size_t len);
+} i2c_slave_ops_t;
+
+int i2c_read_byte(uint8_t saddr, uint8_t reg, uint8_t *value);
+
+int i2c_write_byte(uint8_t saddr, uint8_t reg, uint8_t value);
+
+int i2c_read_bytes(uint8_t saddr, uint8_t reg, uint8_t *buf, size_t len);
+
+int i2c_write_bytes(uint8_t saddr, uint8_t reg, uint8_t *buf, size_t len);
+
+#endif  /* I2C_H */

commit 1f67a28f807d4cf0aba708f3c796997c9e45742e

i2c: Fix debugging outputs printing invalid strings
Fixes: 9521b9e9e8dc ("Add generic I2C operations")
Signed-off-by: Sergiu Moga <sergiu.moga@protonmail.com>

drivers/i2c.c
@@ -7,7 +7,8 @@ int i2c_write_byte(uint8_t saddr, uint8_t reg, uint8_t value)
     msg[0] = reg;
     msg[1] = value;
 
-    pr_debug("%s:%u Writing to saddr %x reg %x value %x", saddr, reg, value);
+    pr_debug("%s:%u Writing to saddr %x reg %x value %x\n", __func__, __LINE__,
+             saddr, reg, value);
 
     return i2c_write_blocking(i2c_default, saddr, msg, sizeof(msg), false);
 }
@@ -16,12 +17,12 @@ int i2c_read_byte(uint8_t saddr, uint8_t reg, uint8_t *value)
 {
     int ret;
 
-    pr_debug("%s:%u Writing to saddr %x reg %x", saddr, reg);
+    pr_debug("%s:%u Writing to saddr %x reg %x\n", __func__, __LINE__, saddr, reg);
     ret = i2c_write_blocking(i2c_default, saddr, &reg, sizeof(reg), false);
     if (ret <= 0)
         return -1;
 
-    pr_debug("%s:%u Reading from saddr %x reg %x", saddr, reg);
+    pr_debug("%s:%u Reading from saddr %x reg %x\n", __func__, __LINE__, saddr, reg);
     ret = i2c_read_blocking(i2c_default, saddr, value, sizeof(*value), false);
     if (ret <= 0)
         return -1;