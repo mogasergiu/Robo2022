2b32352d78afc8cbe4f351d3bfaf763611296ccc
Sergiu Moga <sergiu.moga@protonmail.com>
2022-11-02 15:39:16 +0200

    core0: Calculate positional vector coordinates through LSM6DSOX
    In order to figure out what is physically going on with the robot use
    the accelerometer to figure out the direction in which we are moving
    and the gyroscope to know when the enemy would lift us up.
    
    An `acc_Ry_error` was used since, empirically, we found out that there
    is an error rate between what we expected and what we received on the
    OY axis of the accelerometer.
    
    Signed-off-by: Sergiu Moga <sergiu.moga@protonmail.com>

--- core0.c
+++ core0.c
@@ -6,6 +6,8 @@
 #include <stdio.h>
 #include <debug.h>
 #include <gpio.h>
+#include <math.h>
+#include <stdlib.h>
 
 static gpio_t gpios[] = {
     {
@@ -105,16 +107,86 @@ static int init_all()
 
 extern void core1_main();
 
+float acc_Rx, acc_Ry, acc_Rz, acc_R;
+float gyro_Rx, gyro_Ry, gyro_Rz, gyro_R;
+const float acc_Ry_error = .3f;
+
+static inline void update_position()
+{
+    float volts_accRx, volts_accRy, volts_accRz;
+    float volts_gyroRx, volts_gyroRy, volts_gyroRz;
+    const float acc_volts_0g = 1.45f;
+    const float gyro_volts_0g = 1.23f;
+    const float acc_sensitivity = 0.488f;
+    const float gyro_sensitivity = 0.002f;
+    float cos_accRx, cos_accRy, cos_accRz;
+    float cos_gyroRx, cos_gyroRy, cos_gyroRz;
+    float wGyro = 10.f;
+    volts_accRx = (float) lsm6dsox.acc->get_x() * 3.3f / 65535.f;
+    volts_accRy = (float) lsm6dsox.acc->get_y() * 3.3f / 65535.f;
+    volts_accRz = (float) lsm6dsox.acc->get_z() * 3.3f / 65535.f;
+
+    acc_Rx = (volts_accRx - acc_volts_0g) / acc_sensitivity;
+    acc_Ry = (volts_accRy - acc_volts_0g) / acc_sensitivity;
+    acc_Rz = (volts_accRz - acc_volts_0g) / acc_sensitivity;
+
+    // acc_R = sqrt(acc_Rx * acc_Rx + acc_Ry * acc_Ry + acc_Rz * acc_Rz);
+
+    // cos_accRx = acc_Rx / acc_R;
+    // cos_accRy = acc_Ry / acc_R;
+    // cos_accRz = acc_Rz / acc_R;
+
+    // printf("acc_Rx %f\nacc_Ry %f\nacc_Rz %f\n", acc_Rx, acc_Ry, acc_Rz);
+
+    volts_gyroRx = (float) lsm6dsox.gyro->get_x() * 3.3f / 65535.f;
+    volts_gyroRy = (float) lsm6dsox.gyro->get_y() * 3.3f / 65535.f;
+    volts_gyroRz = (float) lsm6dsox.gyro->get_z() * 3.3f / 65535.f;
+
+    gyro_Rx = (volts_gyroRx - gyro_volts_0g) / gyro_sensitivity;
+    gyro_Ry = (volts_gyroRy - gyro_volts_0g) / gyro_sensitivity;
+    gyro_Rz = (volts_gyroRz - gyro_volts_0g) / gyro_sensitivity;
+
+    // gyro_R = sqrt(gyro_Rx * gyro_Rx + gyro_Ry * gyro_Ry + gyro_Rz * gyro_Rz);
+
+    // cos_gyroRx = gyro_Rx / gyro_R;
+    // cos_gyroRy = gyro_Ry / gyro_R;
+    // cos_gyroRz = gyro_Rz / gyro_R;
+
+    // printf("gyro_Rx %f\ngyro_Ry %f\ngyro_Rz %f\n", gyro_Rx, gyro_Ry, gyro_Rz);
+
+    // Rx = (acc_Rx + gyro_Rx * wGyro) / (1 + wGyro);
+    // Ry = (acc_Ry + gyro_Ry * wGyro) / (1 + wGyro);
+    // Rz = (acc_Rz + gyro_Rz * wGyro) / (1 + wGyro);
+
+    // printf("Rx %f\nRy %f\nRz %f\n", Rx, Ry, Rz);
+}
+
 int main() {
     multicore_launch_core1(core1_main);
     init_all();
 
     while (true) {
         //printf("hi\n");
-        // pr_debug("g.x %d\ng.y %d\ng.z %d\na.x %d\na.y %d\na.z %d\n",
+        // printf("g.x %d\ng.y %d\ng.z %d\na.x %d\na.y %d\na.z %d\n",
         //         lsm6dsox.gyro->get_x(), lsm6dsox.gyro->get_y(), lsm6dsox.gyro->get_z(),
         //         lsm6dsox.acc->get_x(), lsm6dsox.acc->get_y(), lsm6dsox.acc->get_z());
-        sleep_ms(1000);
+        update_position();
+
+        if ((int)acc_Rx != -2)
+            printf("Ox - stanga/dreapta %d\n", (int)acc_Rx);
+
+        if ((int)(acc_Ry - acc_Ry_error) != -3)
+            printf("Oy - fata/spate %d\n", (int)(acc_Ry - acc_Ry_error));
+
+        if ((int)gyro_Rx / 100 != -6)
+            printf("Ox - sus/jos %d\n", (int)gyro_Rx / 100);
+
+        if ((int)gyro_Rz / 100 > -6)
+            printf("Oz - rotatie spre dreapta %d\n", (int)gyro_Rz / 100);
+        else if ((int)gyro_Rz / 100 < -6)
+            printf("Oz - rotatie spre stanga %d\n", (int)gyro_Rz / 100);
+
+        sleep_ms(100);
     }
     return 0;
 }
